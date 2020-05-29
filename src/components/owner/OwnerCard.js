8e3943def5efd39dbcf04a34ef6fbd1ac9f88f7c
Kirk Sudduth <kirksudduth@gmail.com>
2020-05-29 11:25:27 -0500

    added styling to admit animal btn

--- src/components/owner/OwnerCard.js
+++ src/components/owner/OwnerCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const OwnerCard = (props) => {
   return (
-    <div className="card-owner">
+    <div className="card">
       <div className="card-content">
         <h3>
           Name: <span className="card-ownerName">{props.owner.name}</span>
